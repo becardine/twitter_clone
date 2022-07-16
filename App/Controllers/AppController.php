1386a0406c1e4571e69dffd3daad8f144fe5a0a1
becardine <brenda_alves@live.com>
2022-07-16 15:35:48 -0300

    create timeline and list tweet

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -6,23 +6,47 @@ namespace App\Controllers;
 use MF\Controller\Action;
 use MF\Model\Container;
 
-class AppController extends Action {
+class AppController extends Action
+{
 
-    public function timeline() {
+    public function timeline()
+    {
+
+        $this->validaAutenticacao();
+
+        //RECUPERAÇÃO DOS TWEETS
+        $tweet = Container::getModel('Tweet');
+
+        $tweet->__set('id_usuario', $_SESSION['id']);
+        $tweets = $tweet->getAll();
+
+        $this->view->tweets = $tweets;
+
+        $this->render('timeline');
+    }
+
+    public function tweet()
+    {
+
+        $this->validaAutenticacao();
+
+        $tweet = Container::getModel('Tweet');
+
+        $tweet->__set('tweet', $_POST['tweet']);
+        $tweet->__set('id_usuario', $_SESSION['id']);
+
+        $tweet->salvar();
+
+        header('Location: /timeline');
+    }
+
+    public function validaAutenticacao()
+    {
 
         session_start();
 
-        if($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
-            $this->render('timeline');
-        } else {
+        if (!isset($_SESSION['id']) || $_SESSION['id'] == '' || !isset($_SESSION['nome']) || $_SESSION['nome'] == '') {
             header('Location: /?login=error');
         }
-
     }
-
-
-
 }
-
-
-?>
