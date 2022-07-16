9b5aae9469a5ce1c48214ce8f6cdc5cb6b13301b
becardine <brenda_alves@live.com>
2022-07-16 12:05:45 -0300

    create user authentication

--- App/Route.php
+++ App/Route.php
@@ -28,6 +28,21 @@ class Route extends Bootstrap {
 			'controller' => 'indexController',
 			'action' => 'cadastro'
 		);
+		$routes['autenticar'] = array(
+			'route' => '/autenticar',
+			'controller' => 'AuthController',
+			'action' => 'autenticar'
+		);
+		$routes['timeline'] = array(
+			'route' => '/timeline',
+			'controller' => 'AppController',
+			'action' => 'timeline'
+		);
+		$routes['sair'] = array(
+			'route' => '/sair',
+			'controller' => 'AuthController',
+			'action' => 'sair'
+		);
 
 		$this->setRoutes($routes);
 	}
